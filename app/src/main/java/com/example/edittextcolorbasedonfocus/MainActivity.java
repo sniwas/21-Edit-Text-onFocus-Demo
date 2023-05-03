d34f11dd649ad95ff048d2f699bd296a7702947e
sniwas <sniwaserode@gmail.com>
2023-05-03 07:37:46 +0530

    OnFocusEditText

---
+++ app/src/main/java/com/example/edittextcolorbasedonfocus/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.edittextcolorbasedonfocus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+public class MainActivity extends AppCompatActivity {
+    EditText editText;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        editText=findViewById(R.id.et);
+        editText.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+                if(hasFocus) {
+                    editText.setTextColor(getResources().getColor(R.color.red));
+                }
+                else{
+                    editText.setTextColor(getResources().getColor(R.color.black));
+                }
+            }
+        });
+    }
+}
